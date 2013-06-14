39bc90a5b354d802b8ed308a444e35513d2c41f8
Tingish <tingish@gmail.com>
2013-06-14 17:24:48 -0400

    Installed mptt

--- _science/settings.py
+++ _science/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
 	 'south',
+     'mptt',
 )
 
 # A sample logging configuration. The only tangible logging
