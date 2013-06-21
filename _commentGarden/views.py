1f6e9c11b881132e5b5738701eaab21abcc25f27
Ting Wang <tingish@gmail.com>
2013-06-21 16:30:49 -0400

    - home page fix - _commentGarden/views.py change (comment_list is all unpublished SN, missing entry forms) - _content/forms.py changed (imported a few things, not using any of it yet)

--- _commentGarden/views.py
+++ _commentGarden/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.shortcuts import render
+from _content.models import StructureNode
+
+def index(request):
+
+    comment_list = StructureNode.objects.get(isPublished = False)
+    
+    return render(request, '_commentGarden/commentgarden.html', {'comment_list': comment_list,}) #'form':CommentForm()})
--- _science/urls.py
+++ _science/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'homepage.views.home', name='home'),
+     url(r'^$', '_home.views.home', name='home'),
      url(r'^article/', include('_article.urls')),
     # url(r'^_science/', include('_science.foo.urls')),
 
