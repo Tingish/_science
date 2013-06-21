d7eb4c587e23ae0578257abda0a6846d4e9111f7
Ting Wang <tingish@gmail.com>
2013-06-21 10:26:05 -0400

    got rid of early node model.

--- homepage/models.py
+++ homepage/models.py
@@ -2,8 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-
-class node(MPTTModel):
-    type = models.IntegerField()
-    title = models.CharField(max_length=50, unique=True)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
